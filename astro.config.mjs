ea72def0b9bfd1dcab27e31b85243990c0c3b212
Rana Ali <muhammadalisafdera18@gmail.com>
2026-03-14 10:34:31 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://pakistanbill.online',
   integrations: [
     tailwind(),
-    sitemap({
-      changefreq: 'monthly',
-      priority: 0.7,
-      lastmod: new Date(),
-    }),
   ],
   output: 'static',
 });
